a0c146dc45c32e34479b50a2b6e85d33c556486e
Vidhan Bhatt <me@vidhan.io>
2021-11-17 23:02:41 -0500

    add some projs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 
 import "tailwindcss/tailwind.css";
+import "../styles/main.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
