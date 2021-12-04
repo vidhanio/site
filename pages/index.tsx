ef0c8dbf60acc7bc8d34b2e4176b6f52ea757a5e
Vidhan Bhatt <me@vidhan.io>
2021-12-04 16:01:14 -0500

    add some stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ interface GitHubProjectProps {
 function Social({ icon, url, name }: SocialProps) {
   return (
     <a
-      className="flex flex-row items-center justify-center gap-2 text-lg text-blue-500 transition-colors hover:text-blue-400 dark:hover:text-blue-600"
+      className="flex flex-row gap-2 justify-center items-center text-lg text-blue-500 transition-colors hover:text-blue-400 dark:hover:text-blue-600"
       href={url}
     >
       <FontAwesomeIcon icon={icon}></FontAwesomeIcon>
@@ -45,13 +45,13 @@ function Social({ icon, url, name }: SocialProps) {
 function GitHubProject({ url, imageSrc, body }: GitHubProjectProps) {
   return (
     <a href={url}>
-      <div className="flex flex-col items-center justify-center gap-4 p-8 text-lg text-black bg-white w-96 md:w-auto md:max-h-64 rounded-xl">
-        <div className="flex flex-row items-center gap-2">
+      <div className="flex flex-col gap-4 justify-center items-center p-8 w-96 text-lg text-black bg-white rounded-xl md:w-auto md:max-h-64">
+        <div className="flex flex-row gap-2 items-center">
           <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
           <div className="text-sm">/</div>
           <h3 className="text-xl font-bold">{url.split("/").pop()}</h3>
         </div>
-        <div className="flex flex-col items-center gap-4 text-center md:text-left md:flex-row">
+        <div className="flex flex-col gap-4 items-center text-center md:text-left md:flex-row">
           <div className="w-32 h-32">
             <Image
               src={imageSrc}
@@ -67,37 +67,49 @@ function GitHubProject({ url, imageSrc, body }: GitHubProjectProps) {
   );
 }
 
+interface SectionProps {
+  title: string;
+  subtitle?: React.ReactNode;
+  children: React.ReactNode;
+}
+
+function Section({ title, subtitle, children }: SectionProps) {
+  return (
+    <div className="flex flex-col gap-16 justify-center items-center p-16 min-h-screen">
+      <h3 className="text-8xl font-black text-black dark:text-white">
+        {title}
+      </h3>
+      {children}
+    </div>
+  );
+}
+
 class Home extends React.Component {
   render() {
     return (
-      <div className="flex flex-col items-center justify-center w-screen min-h-screen">
-        <div className="flex flex-col items-center justify-center min-h-screen gap-16 p-16">
-          <div className="flex flex-col items-center justify-center gap-4">
-            <h1 className="font-black text-black text-8xl dark:text-white">
-              vidhan
-            </h1>
-            <div className="flex flex-col gap-2 font-bold md:flex-row md:gap-4">
-              <Social
-                icon={faEnvelope}
-                url="mailto:me@vidhan.io"
-                name="me@vidhan.io"
-              ></Social>
-              <Social
-                icon={faGithub}
-                url="https://github.com/vidhanio"
-                name="vidhanio"
-              ></Social>
-              <Social
-                icon={faLinkedin}
-                url="https://linkedin.com/in/vidhanio"
-                name="vidhanio"
-              ></Social>
-              <Social
-                icon={faTwitter}
-                url="https://twitter.com/vidhanio"
-                name="vidhanio"
-              ></Social>
-            </div>
+      <div className="flex flex-col justify-center items-center w-screen min-h-screen">
+        <Section title="vidhan">
+          <div className="flex flex-col gap-2 font-bold md:flex-row md:gap-4">
+            <Social
+              icon={faEnvelope}
+              url="mailto:me@vidhan.io"
+              name="me@vidhan.io"
+            ></Social>
+            <Social
+              icon={faGithub}
+              url="https://github.com/vidhanio"
+              name="vidhanio"
+            ></Social>
+            <Social
+              icon={faLinkedin}
+              url="https://linkedin.com/in/vidhanio"
+              name="vidhanio"
+            ></Social>
+            <Social
+              icon={faTwitter}
+              url="https://twitter.com/vidhanio"
+              name="vidhanio"
+            ></Social>
           </div>
           <a
             href="#projects"
@@ -105,15 +117,15 @@ class Home extends React.Component {
           >
             <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon>{" "}
           </a>
-        </div>
+        </Section>
         <div
           id="projects"
-          className="flex flex-col items-center justify-center min-h-screen gap-16 p-16"
+          className="flex flex-col gap-16 justify-center items-center p-16 min-h-screen"
         >
-          <h1 className="font-black text-black text-8xl dark:text-white">
+          <h1 className="text-8xl font-black text-black dark:text-white">
             projects
           </h1>{" "}
-          <div className="flex flex-col flex-wrap items-center justify-center gap-4 md:flex-row">
+          <div className="flex flex-col flex-wrap gap-4 justify-center items-center md:flex-row">
             <GitHubProject
               url="https://github.com/vidhanio/checkpoint"
               imageSrc={CheckpointImage}
