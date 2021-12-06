09e0dba7a4877bb7728f403ce92432f890271ee7
Vidhan Bhatt <me@vidhan.io>
2021-12-06 12:25:25 -0500

    simplify

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  basePath: "/triple-tac-toe",
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/triple-tac-toe",
-      },
-      {
-        source: "/:path*",
-        destination: "/triple-tac-toe/:path*",
-      },
-    ];
-  },
 };
