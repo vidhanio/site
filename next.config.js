9e402e6812aafbd505647670c948953c78017bd6
Vidhan Bhatt <me@vidhan.io>
2021-12-06 10:22:48 -0500

    maybe.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: `/`,
+      },
+      {
+        source: "/triple-tac-toe/:path*",
+        destination: `triple-tac-toe.vidhan.io`,
+      },
+      {
+        source: "/resume/:path*",
+        destination: `resume.vidhan.io`,
+      },
+    ];
+  },
+};
