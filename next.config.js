2d4f2ffd7e28b4ddd10573d38432db0371e14aa2
Vidhan Bhatt <me@vidhan.io>
2021-12-06 11:28:06 -0500

    add rewritez

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/triple-tac-toe",
+        destination: `https://triple-tac-toe.vidhan.io/triple-tac-toe`,
+      },
+      {
+        source: "/triple-tac-toe/:path*",
+        destination: `https://triple-tac-toe.vidhan.io/triple-tac-toe/:path*`,
+      },
+      {
+        source: "/resume",
+        destination: `https://resume.vidhan.io/resume`,
+      },
+      {
+        source: "/resume/:path*",
+        destination: `https://resume.vidhan.io/resume/:path*`,
+      },
+    ];
+  },
 };
