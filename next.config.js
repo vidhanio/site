0a7332e013f191b9b1c8535a146b591f18c3ac73
Vidhan Bhatt <me@vidhan.io>
2021-12-06 10:41:49 -0500

    remove everythin

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/triple-tac-toe/:path*",
-        destination: `https://triple-tac-toe.vidhan.io/triple-tac-toe`,
-      },
-      {
-        source: "/resume/:path*",
-        destination: `https://resume.vidhan.io`,
-      },
-    ];
-  },
 };
