5093e0153cc1e1462cce4ebf485efd79e00adc7e
Vidhan Bhatt <me@vidhan.io>
2021-12-06 12:25:27 -0500

    simplify

--- next.config.js
+++ next.config.js
@@ -4,20 +4,8 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/triple-tac-toe",
-        destination: "https://triple-tac-toe.vidhan.io/triple-tac-toe",
-      },
-      {
-        source: "/triple-tac-toe/:path*",
-        destination: "https://triple-tac-toe.vidhan.io/triple-tac-toe/:path*",
-      },
-      {
         source: "/resume",
         destination: "https://resume.vidhan.io/resume",
-      },
-      {
-        source: "/resume/:path*",
-        destination: "https://resume.vidhan.io/resume/:path*",
       },
     ];
   },
