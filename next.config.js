a0057067bbb499cd66b9b997808f4b6fd58557bb
Vidhan Bhatt <me@vidhan.io>
2021-12-06 10:30:42 -0500

    explicit?

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ module.exports = {
     return [
       {
         source: "/triple-tac-toe/:path*",
-        destination: `https://triple-tac-toe.vidhan.io`,
+        destination: `https://triple-tac-toe.vidhan.io/:path*'`,
       },
       {
         source: "/resume/:path*",
-        destination: `https://resume.vidhan.io`,
+        destination: `https://resume.vidhan.io/:path*'`,
       },
     ];
   },
