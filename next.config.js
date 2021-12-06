783473469274b272a67e3df4d4f6cf42e4f984b8
Vidhan Bhatt <me@vidhan.io>
2021-12-06 12:13:21 -0500

    switch to quotz

--- next.config.js
+++ next.config.js
@@ -5,19 +5,19 @@ module.exports = {
     return [
       {
         source: "/triple-tac-toe",
-        destination: `https://triple-tac-toe.vidhan.io/triple-tac-toe`,
+        destination: "https://triple-tac-toe.vidhan.io/triple-tac-toe",
       },
       {
         source: "/triple-tac-toe/:path*",
-        destination: `https://triple-tac-toe.vidhan.io/triple-tac-toe/:path*`,
+        destination: "https://triple-tac-toe.vidhan.io/triple-tac-toe/:path*",
       },
       {
         source: "/resume",
-        destination: `https://resume.vidhan.io/resume`,
+        destination: "https://resume.vidhan.io/resume",
       },
       {
         source: "/resume/:path*",
-        destination: `https://resume.vidhan.io/resume/:path*`,
+        destination: "https://resume.vidhan.io/resume/:path*",
       },
     ];
   },
