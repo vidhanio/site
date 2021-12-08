388aa9dd1a4230136eb3e4b7170c74d337a9eea1
Vidhan Bhatt <me@vidhan.io>
2021-12-07 20:21:37 -0500

    add better padding

--- components/layouts/blog/main.tsx
+++ components/layouts/blog/main.tsx
@@ -4,7 +4,7 @@ function BlogMainLayout({
   children?: React.ReactNode;
 }): JSX.Element {
   return (
-    <main className="flex flex-col gap-2 px-32 pb-32 w-full xl:px-96">
+    <main className="flex flex-col gap-2 px-16 pb-16 w-full md:px-32 md:pb-32 xl:px-96">
       {children}
     </main>
   );
