5895d1b2b1df6ddfb8e549e45537beb9b9c19d0b
Vidhan Bhatt <me@vidhan.io>
2021-12-07 20:19:48 -0500

    add fontFamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,25 @@ module.exports = {
       minHeight: {
         "3/4-screen": "75vh",
       },
+      fontFamily: {
+        sans: [
+          "Poppins",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "'Segoe UI'",
+          "Roboto",
+          "'Helvetica Neue'",
+          "Arial",
+          "'Noto Sans'",
+          "sans-serif",
+          "'Apple Color Emoji'",
+          "'Segoe UI Emoji'",
+          "'Segoe UI Symbol'",
+          "'Noto Color Emoji'",
+        ],
+      },
     },
   },
   variants: {
