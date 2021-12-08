d4bb2d4fff90c238739b6822b00e7fab7b06d622
Vidhan Bhatt <me@vidhan.io>
2021-12-08 09:08:42 -0500

    change some configfiles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
