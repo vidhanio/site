138d6c3c2524f674eea8c86fe2058809a4b229cc
Vidhan Bhatt <me@vidhan.io>
2021-12-08 19:58:05 -0500

    add some  config stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 
 module.exports = {
-  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media",
   theme: {
