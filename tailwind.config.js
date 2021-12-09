d88beb7664c71512b1bccc69718a7e39f3660890
Vidhan Bhatt <me@vidhan.io>
2021-12-09 11:29:33 -0500

    add css to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.css",
   ],
   darkMode: "media",
   theme: {
