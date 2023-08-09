c48991f4cd1571c97a6296c679eb569a52e5b484
Vidhan Bhatt <me@vidhan.io>
2023-08-09 18:15:55 -0400

    style: make ring component less intrusive

--- src/components/footer/ring.rs
+++ src/components/footer/ring.rs
@@ -13,25 +13,23 @@ const LINKS: [(Icon, &str); 3] = [
 
 pub fn ring() -> Node {
     html! {
-        <div class="w-3/4 flex flex-col pt-4 px-4 gap-4 border-t-2 border-indigo-300 dark:border-indigo-700">
-            <a
-                href="https://ring.simonwu.dev"
-                class="text-slate-400 dark:text-slate-600 hover:text-slate-500 transition-colors"
-            >
-                "cozyring"
-            </a>
-            <div class="flex flex-row flex-wrap items-center justify-center gap-4 text-slate-400 dark:text-slate-600">
-                { LINKS.into_iter().map(|(icon, href)| {
-                    html! {
-                        <a
-                            href=href
-                            class="text-slate-400 dark:text-slate-600 hover:text-slate-500 transition-colors"
-                        >
-                            {icon(Some("h-6"))}
-                        </a>
-                    }
-                }) }
-            </div>
+        <a
+            href="https://ring.simonwu.dev"
+            class="text-slate-400 dark:text-slate-600 hover:text-slate-500 transition-colors"
+        >
+            "cozyring"
+        </a>
+        <div class="flex flex-row flex-wrap items-center justify-center gap-4 text-slate-400 dark:text-slate-600">
+            { LINKS.into_iter().map(|(icon, href)| {
+                html! {
+                    <a
+                        href=href
+                        class="text-slate-400 dark:text-slate-600 hover:text-slate-500 transition-colors"
+                    >
+                        {icon(Some("h-6"))}
+                    </a>
+                }
+            }) }
         </div>
     }
 }
